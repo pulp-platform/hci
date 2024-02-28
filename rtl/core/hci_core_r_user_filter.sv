6639fe28655a414c62ab25f5c9c73727723990a0
Francesco Conti <f.conti@unibo.it>
2024-02-28 23:10:39 +0000

    Add asserts for hci_core_r_user_filter
    The hci_core_r_user_filter works *only* if it is positioned at the 1-cycle latency boundary, i.e.,
    the boundary in a cluster where we are guaranteed that a grant on a read results in a response in
    the following cycle. Positioning it in another place results in hard-to-debug problems, typically
    showing up as r_valid's never being taken or being served to the wrong master by a OoO mux or a
    dynamic mux.
    These asserts try to avoid this scenario!

--- rtl/core/hci_core_r_user_filter.sv
+++ rtl/core/hci_core_r_user_filter.sv
@@ -60,4 +60,30 @@ module hci_core_r_user_filter #(
     end
   end
 
+/*
+ * The hci_core_r_user_filter works *only* if it is positioned at the 1-cycle latency boundary, i.e.,
+ * the boundary in a cluster where we are guaranteed that a grant on a read results in a response in
+ * the following cycle. Positioning it in another place results in hard-to-debug problems, typically
+ * showing up as r_valid's never being taken or being served to the wrong master by a OoO mux or a
+ * dynamic mux.
+ * These asserts try to avoid this scenario!
+ */
+`ifndef SYNTHESIS
+  // gnt=1 & wen=1 => the following cycle r_valid=1
+  property p_gnt_wen_high_then_r_valid_high_next_cycle;
+    @(posedge clk_i) (tcdm_master.gnt && tcdm_master.wen) |-> ##1 tcdm_master.r_valid;
+  endproperty
+
+  assert_gnt_wen_high_then_r_valid_high_next_cycle: assert property (p_gnt_wen_high_then_r_valid_high_next_cycle)
+    else $warning("`r_valid` did not follow `gnt` by 1 cycle in a read: are you sure the `r_user` filter is at the 1-cycle latency boundary?");
+
+  // gnt=0 => the following cycle r_valid=0
+  property p_gnt_low_then_r_valid_low_next_cycle;
+    @(posedge clk_i) (!tcdm_master.gnt) |-> ##1 !tcdm_master.r_valid;
+  endproperty
+
+  assert_gnt_low_then_r_valid_low_next_cycle: assert property (p_gnt_low_then_r_valid_low_next_cycle)
+    else $warning("`r_valid` did not follow `gnt` by 1 cycle in a read: are you sure the `r_user` filter is at the 1-cycle latency boundary?");
+`endif
+
 endmodule // hci_core_r_user_filter
