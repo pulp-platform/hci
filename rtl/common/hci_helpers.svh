3905c047996ff4c2347ffdaf9b2b66abc63c1011
Francesco Conti <FrancescoConti@users.noreply.github.com>
2024-04-24 20:04:15 +0200

    Manage the special case of FPGA targets in hci_helpers.svh

--- rtl/common/hci_helpers.svh
+++ rtl/common/hci_helpers.svh
@@ -16,25 +16,46 @@
 `ifndef __HCI_HELPERS__
 `define __HCI_HELPERS__
 
+// Manage the special case of FPGA targets
+`ifdef TARGET_FPGA
+  `define HCI_TARGET_FPGA
+`elsif PULP_FPGA_EMUL
+ `define HCI_TARGET_FPGA
+`elsif FPGA_EMUL
+ `define HCI_TARGET_FPGA
+`elsif XILINX
+ `define HCI_TARGET_FPGA
+`endif
+
 // Helper `defines to get internal sizes. Interestingly, the code for synthesis
 // does not (consistently) work in simulation, and vice versa - although they
 // have exactly the same purpose.
 `ifndef SYNTHESIS
-  `define HCI_SIZE_GET_DW(x)  x.DW
-  `define HCI_SIZE_GET_AW(x)  x.AW
-  `define HCI_SIZE_GET_BW(x)  x.BW
-  `define HCI_SIZE_GET_UW(x)  x.UW
-  `define HCI_SIZE_GET_IW(x)  x.IW
-  `define HCI_SIZE_GET_EW(x)  x.EW
-  `define HCI_SIZE_GET_EHW(x) x.EHW
-`else /* `ifdef SYNTHESIS */
-  `define HCI_SIZE_GET_DW(x)  $bits(x.data)
-  `define HCI_SIZE_GET_AW(x)  $bits(x.add)
-  `define HCI_SIZE_GET_BW(x)  $bits(x.be)
-  `define HCI_SIZE_GET_UW(x)  $bits(x.user)
-  `define HCI_SIZE_GET_IW(x)  $bits(x.id)
-  `define HCI_SIZE_GET_EW(x)  $bits(x.ecc)
-  `define HCI_SIZE_GET_EHW(x) $bits(x.ereq)
-`endif /* `ifdef SYNTHESIS */
+  `define HCI_SIZE_GET_DW(x)  (x.DW)
+  `define HCI_SIZE_GET_AW(x)  (x.AW)
+  `define HCI_SIZE_GET_BW(x)  (x.BW)
+  `define HCI_SIZE_GET_UW(x)  (x.UW)
+  `define HCI_SIZE_GET_IW(x)  (x.IW)
+  `define HCI_SIZE_GET_EW(x)  (x.EW)
+  `define HCI_SIZE_GET_EHW(x) (x.EHW)
+`else /* SYNTHESIS */
+  `ifdef HCI_TARGET_FPGA
+    `define HCI_SIZE_GET_DW(x)  (x.DW)
+    `define HCI_SIZE_GET_AW(x)  (x.AW)
+    `define HCI_SIZE_GET_BW(x)  (x.BW)
+    `define HCI_SIZE_GET_UW(x)  (x.UW)
+    `define HCI_SIZE_GET_IW(x)  (x.IW)
+    `define HCI_SIZE_GET_EW(x)  (x.EW)
+    `define HCI_SIZE_GET_EHW(x) (x.EHW)
+  `else /* not HCI_TARGET FPGA */
+    `define HCI_SIZE_GET_DW(x)  ($bits(x.data))
+    `define HCI_SIZE_GET_AW(x)  ($bits(x.add))
+    `define HCI_SIZE_GET_BW(x)  ($bits(x.be))
+    `define HCI_SIZE_GET_UW(x)  ($bits(x.user))
+    `define HCI_SIZE_GET_IW(x)  ($bits(x.id))
+    `define HCI_SIZE_GET_EW(x)  ($bits(x.ecc))
+    `define HCI_SIZE_GET_EHW(x) ($bits(x.ereq))
+  `endif
+`endif
 
 `endif /* `ifndef __HCI_HELPERS__ */
