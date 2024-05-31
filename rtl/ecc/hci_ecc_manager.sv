9aafa396b4052554b8290d70e741384241535d16
Luigi Ghionda <luigi.ghionda2@unibo.it>
2024-05-31 17:03:44 +0200

    Update authors

--- rtl/ecc/hci_ecc_manager.sv
+++ rtl/ecc/hci_ecc_manager.sv
@@ -1,14 +1,17 @@
-// Copyright 2024 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the "License"); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-//
-// ECC hci manager
+/*
+ * hci_ecc_manager.sv
+ * Luigi Ghionda <luigi.ghionda2@unibo.it>
+ *
+ * Copyright 2024 ETH Zurich and University of Bologna.
+ * Copyright and related rights are licensed under the Solderpad Hardware
+ * License, Version 0.51 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ * http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+ * or agreed to in writing, software, hardware and materials distributed under
+ * this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+*/
 
 module hci_ecc_manager
   import hci_ecc_manager_reg_pkg::*;
@@ -96,4 +99,4 @@ module hci_ecc_manager
   assign hw2reg.metadata_uncorrectable_errors.d = reg2hw.metadata_uncorrectable_errors.q + meta_uncorrectable_err_num;
   assign hw2reg.metadata_uncorrectable_errors.de = |(meta_uncorrectable_err_i);
 
-endmodule
+endmodule // hci_ecc_manager
